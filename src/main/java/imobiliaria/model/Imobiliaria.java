e4fa3e3609afb6a84ec738b006549f646bc02d79
Débora Alves <deboraalves007@hotmail.com>
2026-03-21 12:08:11 -0300

    add Imobiliaria

--- src/main/java/imobiliaria/model/Imobiliaria.java
+++ src/main/java/imobiliaria/model/Imobiliaria.java
@@ -1,10 +1,59 @@
 package main.java.imobiliaria.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Imobiliaria {
 
-    List<Imovel> imoveisParaVenda;
-    List<Imovel> imoveisParaLocacao;
+    private Double comissoes;
 
+    private List<Imovel> imoveis = new ArrayList<>();
+
+    private List<Funcionario> funcionarios = new ArrayList<>();
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public void adicionarComissao(Double valor) {
+        this.comissoes += valor;
+    }
+
+    public void adicionarImovel(Imovel imovel) {
+        this.imoveis.add(imovel);
+    }
+
+    public void removerImovel(Imovel imovel) {
+        this.imoveis.remove(imovel);
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        this.funcionarios.add(funcionario);
+    }
+
+    public void removerFuncionario(Funcionario funcionario) {
+        this.funcionarios.remove(funcionario);
+    }
+
+    public void adicionarCliente(Cliente cliente) {
+        this.clientes.add(cliente);
+    }
+
+    public void removerCliente(Cliente cliente) {
+        this.clientes.remove(cliente);
+    }
+
+    public Double getComissoes() {
+        return comissoes;
+    }
+
+    public List<Imovel> getImoveis() {
+        return imoveis;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
 }
