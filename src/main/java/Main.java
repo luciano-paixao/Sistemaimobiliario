7e9508b7f6664960f877f414773730f71bd97fd9
Marcus Gabriel <130609450+marcozzo@users.noreply.github.com>
2026-03-23 12:35:00 -0300

    classe Main executa o método main() da classe Sistema

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,27 @@ package main.java;
 import main.java.imobiliaria.model.Cliente;
 import main.java.imobiliaria.model.Funcionario;
 import main.java.imobiliaria.model.Imobiliaria;
+import main.java.imobiliaria.model.Sistema;
 
 public class Main {
     public static void main(String[] args) {
-        Imobiliaria i = new Imobiliaria();
+        Sistema.main(new String[]{});
 
-        for (int j = 0; j < 2; j++) {
-            i.cadastarCliente();
-        }
-
-        for (Cliente c : i.getClientes()) {
-            System.out.println(c);
-        }
-
-        for (Funcionario f : i.getFuncionarios()) {
-            System.out.println(f);
-        }
+        // VOSSA VERSÃO
+//    public static void main(String[] args) {
+//        Imobiliaria i = new Imobiliaria();
+//
+//        for (int j = 0; j < 2; j++) {
+//            i.cadastarCliente();
+//        }
+//
+//        for (Cliente c : i.getClientes()) {
+//            System.out.println(c);
+//        }
+//
+//        for (Funcionario f : i.getFuncionarios()) {
+//            System.out.println(f);
+//        }
+//    }
     }
 }
